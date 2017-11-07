044c8850dd79a70e35f21733c4745549711eb53c
maxdosme <233705835@qq.com>
2017-11-07 09:23:32 +0800

    App.vue modify

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    
+    <div class="header">I am Header!</div>
+    <div class="tab">I am Tab</div>
+    <div class="content">I am Content</div>
   </div>
 </template>
 
