6b712b93f1a2110a5289d90934dbf553e7838fc7
maxdosme <233705835@qq.com>
2017-11-16 16:24:28 +0800

    goods左侧导航样式开发

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,10 +3,10 @@
         <!-- 左侧导航 -->
         <div class="menu-wrapper">
             <ul>
-                <li v-for="item in goods">
-                    <span class="text">
-                        <!-- 添加判断type是否大于0 -->
-                        <span v-show="item.type>0" class="icon"></span>{{ item.name }}
+                <li v-for="item in goods" class="menu-item">
+                    <span class="text border-1px">
+                        <!-- 添加判断type是否大于0的显示 -->
+                        <span class="icon" v-show="item.type>0" :class="iconClassMap[item.type]"></span>{{ item.name }}
                     </span>
                 </li>
             </ul>
@@ -50,6 +50,9 @@ import axios from 'axios';
 
 <style lang="stylus" rel="stylesheet/stylus">
 
+//  引入mixin.styl
+@import "../../common/stylus/mixin.styl";
+
     .goods
         position: absolute
         display:flex
@@ -62,6 +65,38 @@ import axios from 'axios';
             // 兼容性写法需要独立在写一个宽度，不然安卓显示错误
             width: 80px
             background: #f3f5f7
+            .menu-item
+                display: table
+                width: 56px
+                height: 54px
+                line-height: 14px
+                padding: 0 12px
+                .icon
+                    display: inline-block
+                    vertical-align: top
+                    width: 12px
+                    height: 12px
+                    margin-right: 2px
+                    background-size: 12px 12px
+                    background-repeat: no-repeat
+                    // 定义折扣图片class 
+                    &.decrease
+                        bg-image('decrease_3')
+                    &.discount
+                        bg-image('discount_3')
+                    &.gurantee
+                        bg-image('guarantee_3')
+                    &.invoice
+                        bg-image('invoice_3')
+                    &.special
+                        bg-image('special_3')
+                .text
+                    display: table-cell
+                    width: 56px
+                    // table布局中的垂直居中
+                    vertical-align: middle
+                    border-1px(rgba(7,17,27,0.1))
+                    font-size: 12px
         .foods-wrapper
             flex: 1
 </style>
