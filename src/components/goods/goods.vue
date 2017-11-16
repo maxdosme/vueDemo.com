c1227b52c5891a8a13dfcadfdfcba5fec9665a9e
maxdosme <233705835@qq.com>
2017-11-16 15:44:29 +0800

    制作goods组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,69 @@
 <template>
-    <div>I am Goods</div>
+    <div class="goods">
+        <!-- 左侧导航 -->
+        <div class="menu-wrapper">
+            <ul>
+                <li v-for="item in goods">
+                    <span class="text">
+                        <!-- 添加判断type是否大于0 -->
+                        <span v-show="item.type>0" class="icon"></span>{{ item.name }}
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <!-- 右侧内容 -->
+        <div class="foods-wrapper"></div>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {};
+
+//  引入axios
+import axios from 'axios';
+    export default {
+        // 通过vue-route传数据
+        props: {
+            seller: {
+                type: Object
+            }
+        },
+        //  绑定goods 赋值给data
+        data() {
+            return {
+                goods: []
+            };
+        },
+        // 使用钩子函数创建
+        created() {
+            // 创建图片数组
+                this.iconClassMap = ['decrease', 'discount', 'special', 'invoice', 'gurantee'];
+                // console.log(this.iconClassMap);
+
+            // axios获取json数据
+            axios.get('static/data.json').then((res) => {
+                this.goods = res.data.goods;
+                // console.log(this.goods);
+            });
+        }
+    };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
 
+    .goods
+        position: absolute
+        display:flex
+        width: 100%
+        top: 174px
+        bottom: 46px
+        overflow: hidden
+        .menu-wrapper
+            flex: 0 0 80px
+            // 兼容性写法需要独立在写一个宽度，不然安卓显示错误
+            width: 80px
+            background: #f3f5f7
+        .foods-wrapper
+            flex: 1
 </style>
 
 
